d39a294c35039dd0f3e6f80de15d4ae84b32b4f0
Ewerton da Silva Cajado <ewertoncajado@hotmail.com>
2022-08-11 09:17:15 +0100

    lista de tarefas
    sigo nos ajustes da famosa to-do list. :+1:

---
+++ lista de tarefas/.history/scripts/script_20220811065835.js
@@ -0,0 +1,69 @@
+let inputNewTask = document.querySelector('#inputNewTask');
+let buttonAddTask = document.querySelector('#buttonAddTask');
+let tasksList = document.querySelector('#tasksList');
+
+inputNewTask.addEventListener('keypress', (e) => {
+
+    if(e.keyCode == 13) {
+        let task = { 
+            name: inputNewTask.value,
+            id: generateId(),
+        }
+        addTask(task);
+    }
+});
+
+/*buttonAddTask.addEventListener('click', (e) => {
+    
+    let task = { 
+        name: inputNewTask.value,
+        id: generateId(),
+    }
+    addTask(task);
+
+});*/
+
+function generateId() {
+    return Math.floor(Math.random() * 3000);
+}
+
+function addTask(task) {
+    let li = createTagLI(task);
+    tasksList.appendChild(li);
+    inputNewTask.value = '';
+
+}
+
+
+function createTagLI(task) {
+
+    let li = document.createElement('li');
+
+    let span = document.createElement('span');
+    span.classList.add('textTask');
+    span.innerHTML = task.name;
+
+    let div = document.createElement('div');
+
+    let buttonEdit = document.createElement('button');
+    buttonEdit.classList.add('actionButton');
+    buttonEdit.innerHTML = '<i class="fa fa-pencil"></i>';
+    buttonEdit.setAttribute('onclick', 'edit(' + task.id+')');
+
+    let buttonRemove = document.createElement('button');
+    buttonRemove.classList.add('actionButton');
+    buttonRemove.innerHTML = '<i class="fa fa-trash"></i>';
+    buttonRemove.setAttribute('onclick', 'remove(' + task.id+')');
+
+
+    div.appendChild(buttonEdit);
+    div.appendChild(buttonRemove);
+
+    li.appendChild(span);
+    li.appendChild(div);
+    
+    return li;
+
+}
+
+edit()
